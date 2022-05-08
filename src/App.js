6821beea2c3ac9faf0d80c203d83fd49303fb171
mgmaxi <maximiliano.ghirardi@gmail.com>
2022-05-08 04:07:01 -0300

    Add Row component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 import Header from './layouts/Header/Header.js';
+import Row from './components/Row/Row.js';
+
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <main>
+        <Row title={'Top'}></Row>
+        <Row title={'Terror'}></Row>
+        <Row title={'Original'}></Row>
+      </main>
     </div>
   );
 }
---
+++ src/components/Row/Row.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import './row.css';
+
+const Row = ({ title }) => {
+  return (
+    <>
+      <h2 className="title">{title}</h2>
+
+      <div
+        className="card-image-container"
+        style={{
+          scrollbarWidth: 'none',
+        }}
+      >
+        <img
+          src="https://occ-0-5397-1740.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABcGjbUhaceM5B41UpIIKXMI9cq6Xjp2bgDoemIQ6l_MqR8v1t8KWLDhPLwD5OpR6gNEt1_5KFeZ7Gn4jcB17IAOSCDWI45eB9uOSkrpnrDD6pgJBx2uwqdcGCvbnP2F9-Y9v.jpg?r=d51"
+          alt="Movie card"
+          className="card-image"
+        />
+        <img
+          src="https://occ-0-5397-1740.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABcGjbUhaceM5B41UpIIKXMI9cq6Xjp2bgDoemIQ6l_MqR8v1t8KWLDhPLwD5OpR6gNEt1_5KFeZ7Gn4jcB17IAOSCDWI45eB9uOSkrpnrDD6pgJBx2uwqdcGCvbnP2F9-Y9v.jpg?r=d51"
+          alt="Movie card"
+          className="card-image"
+        />
+        <img
+          src="https://occ-0-5397-1740.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABcGjbUhaceM5B41UpIIKXMI9cq6Xjp2bgDoemIQ6l_MqR8v1t8KWLDhPLwD5OpR6gNEt1_5KFeZ7Gn4jcB17IAOSCDWI45eB9uOSkrpnrDD6pgJBx2uwqdcGCvbnP2F9-Y9v.jpg?r=d51"
+          alt="Movie card"
+          className="card-image"
+        />
+        <img
+          src="https://occ-0-5397-1740.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABcGjbUhaceM5B41UpIIKXMI9cq6Xjp2bgDoemIQ6l_MqR8v1t8KWLDhPLwD5OpR6gNEt1_5KFeZ7Gn4jcB17IAOSCDWI45eB9uOSkrpnrDD6pgJBx2uwqdcGCvbnP2F9-Y9v.jpg?r=d51"
+          alt="Movie card"
+          className="card-image"
+        />
+        <img
+          src="https://occ-0-5397-1740.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABcGjbUhaceM5B41UpIIKXMI9cq6Xjp2bgDoemIQ6l_MqR8v1t8KWLDhPLwD5OpR6gNEt1_5KFeZ7Gn4jcB17IAOSCDWI45eB9uOSkrpnrDD6pgJBx2uwqdcGCvbnP2F9-Y9v.jpg?r=d51"
+          alt="Movie card"
+          className="card-image"
+        />
+        <img
+          src="https://occ-0-5397-1740.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABcGjbUhaceM5B41UpIIKXMI9cq6Xjp2bgDoemIQ6l_MqR8v1t8KWLDhPLwD5OpR6gNEt1_5KFeZ7Gn4jcB17IAOSCDWI45eB9uOSkrpnrDD6pgJBx2uwqdcGCvbnP2F9-Y9v.jpg?r=d51"
+          alt="Movie card"
+          className="card-image"
+        />
+        <img
+          src="https://occ-0-5397-1740.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABcGjbUhaceM5B41UpIIKXMI9cq6Xjp2bgDoemIQ6l_MqR8v1t8KWLDhPLwD5OpR6gNEt1_5KFeZ7Gn4jcB17IAOSCDWI45eB9uOSkrpnrDD6pgJBx2uwqdcGCvbnP2F9-Y9v.jpg?r=d51"
+          alt="Movie card"
+          className="card-image"
+        />
+        <img
+          src="https://occ-0-5397-1740.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABcGjbUhaceM5B41UpIIKXMI9cq6Xjp2bgDoemIQ6l_MqR8v1t8KWLDhPLwD5OpR6gNEt1_5KFeZ7Gn4jcB17IAOSCDWI45eB9uOSkrpnrDD6pgJBx2uwqdcGCvbnP2F9-Y9v.jpg?r=d51"
+          alt="Movie card"
+          className="card-image"
+        />
+        <img
+          src="https://occ-0-5397-1740.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABcGjbUhaceM5B41UpIIKXMI9cq6Xjp2bgDoemIQ6l_MqR8v1t8KWLDhPLwD5OpR6gNEt1_5KFeZ7Gn4jcB17IAOSCDWI45eB9uOSkrpnrDD6pgJBx2uwqdcGCvbnP2F9-Y9v.jpg?r=d51"
+          alt="Movie card"
+          className="card-image"
+        />
+      </div>
+    </>
+  );
+};
+
+export default Row;
